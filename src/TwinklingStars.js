ca647ed8714a83ca310b8ed5cbbcb174cff66c53
diinoprp <diinoprp@gmail.com>
2019-07-20 19:12:54 -0300

    Fixing background in landscape mode

--- src/TwinklingStars.js
+++ src/TwinklingStars.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-export default function TwinklingStars(props) {
+export default function TwinklingStars() {
   return (
     <>
       <div className="stars" />
