4ca9e71df8faa03527d5be2742c1912ed859af72
diinoprp <diinoprp@gmail.com>
2019-07-21 14:48:38 -0300

    Updates

---
+++ precache-manifest.b220f5574bb4de4b63fb358aa62017be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da2cde3c4eed8c0dea5e0423b2fa659",
+    "url": "/swapi-planets-react/index.html"
+  },
+  {
+    "revision": "f550e162fc1f025e3533",
+    "url": "/swapi-planets-react/static/css/main.35f80fe7.chunk.css"
+  },
+  {
+    "revision": "17715077e19b46965b9d",
+    "url": "/swapi-planets-react/static/js/2.efb316c2.chunk.js"
+  },
+  {
+    "revision": "f550e162fc1f025e3533",
+    "url": "/swapi-planets-react/static/js/main.53028e49.chunk.js"
+  },
+  {
+    "revision": "46eb2ffc451b31832507",
+    "url": "/swapi-planets-react/static/js/runtime~main.e53256db.js"
+  },
+  {
+    "revision": "4a0dffa736fb547fe0992a6c82d21eab",
+    "url": "/swapi-planets-react/static/media/stars.4a0dffa7.webp"
+  },
+  {
+    "revision": "8abb10dfd03f21f98c8ae95e341778dc",
+    "url": "/swapi-planets-react/static/media/twinkling.8abb10df.webp"
+  }
+]);
